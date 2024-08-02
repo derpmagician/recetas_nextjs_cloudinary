50035e8767547616da615616d2ce0373f9e629d5
derpmagician <renzin@gmail.com>
2024-08-02 08:09:55 -0500

    Updates

--- server/server-reference-manifest.js
+++ server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\"node\":{\"461db49837a97e08e72b3bfc30064896a1fb5ee9\":{\"workers\":{\"app/meals/share/page\":\"89218\"},\"layer\":{\"app/meals/share/page\":\"action-browser\"}}},\"edge\":{},\"encryptionKey\":\"d9v+k+G73CGK/m1gNmZnqZzjfrp7MOhLBA5XEGFHMlQ=\"}"
+self.__RSC_SERVER_MANIFEST="{\"node\":{\"461db49837a97e08e72b3bfc30064896a1fb5ee9\":{\"workers\":{\"app/meals/share/page\":\"89218\"},\"layer\":{\"app/meals/share/page\":\"action-browser\"}}},\"edge\":{},\"encryptionKey\":\"UgvcJsTL/RSf6dUFDCimsI30a8X9HBlOjhPfv39wnmQ=\"}"
--- static/_l9bXle3Mi3gjej6ITIVb/_ssgManifest.js
+++
@@ -1,1 +0,0 @@
-self.__SSG_MANIFEST=new Set(["\u002Fmeals\u002F[mealSlug]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
