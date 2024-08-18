2061533aaf29aa830ad597edb50e2d1014702535
derpmagician <renzin@gmail.com>
2024-08-17 19:54:29 -0500

    limpiando imports

--- app/user/page.js
+++ app/user/page.js
@@ -1,6 +1,4 @@
 // app/user/page.js
-import { useEffect, useState } from 'react';
-import { supabase } from '@/lib/supabase';
 // import UserProfile from '@/components/users/UserProfile';
 const UserPage = () => {
   return (
